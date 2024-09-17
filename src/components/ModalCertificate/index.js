01bbc0c8524c6717f415eeba6242ded4962e6b9c
Netanel Mazuz <netazuz@gmail.com>
2024-09-17 23:55:57 +0300

    fix craco.config.js

--- src/components/ModalCertificate/index.js
+++ src/components/ModalCertificate/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import CrumpleEffect from 'components/CrumpleEffect';
 import './index.css';
 
 function ModalCertificate({ showModal, setShowModal, alt, imgSrc }) {
-    console.log(imgSrc);
+    
     return (
         <div className={`modal-certificate ${showModal ? 'visible' : ''}`}>
+            <CrumpleEffect />
             {showModal ? (
-                <img src={imgSrc} alt={alt} />
+                <div className="modal-certificate__container">
+                    {/* Close button that triggers the modal to close */}
+                    <a href="#" className="close-thick" onClick={(e) => {
+                        e.preventDefault();
+                        setShowModal(false);}
+                        }>
+                        &times; {/* You can use '&times;' for a close 'X' symbol */}
+                    </a>
+                    <img src={imgSrc} alt={alt} />
+                </div>
             ) : null}
         </div>
     );
