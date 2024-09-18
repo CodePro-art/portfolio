abef9a8ecb86b0f0ffa0931001dc7fd31acd9c3c
Netanel Mazuz <netazuz@gmail.com>
2024-09-18 10:59:14 +0300

    add modal

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,5 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
   style: {
     postcss: {
@@ -17,6 +19,23 @@ module.exports = {
           },
         }),
       ],
+    },
+  },
+  webpack: {
+    configure: (webpackConfig) => {
+      const miniCssExtractPluginIndex = webpackConfig.plugins.findIndex(
+        (plugin) => plugin instanceof MiniCssExtractPlugin
+      );
+
+      if (miniCssExtractPluginIndex !== -1) {
+        webpackConfig.plugins[miniCssExtractPluginIndex] = new MiniCssExtractPlugin({
+          filename: '[name].css',
+          chunkFilename: '[name].chunk.css',
+          ignoreOrder: true, // Ignore CSS order conflicts
+        });
+      }
+
+      return webpackConfig;
     },
   },
 };
