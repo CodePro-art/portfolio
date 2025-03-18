ec49124b04a59f96dddf8cbee31512f5dd30091e
Netanel Mazuz <netazuz@gmail.com>
2025-03-18 13:53:12 +0200

    add icon-500.png to public space

--- src/components/Monogram/index.js
+++ src/components/Monogram/index.js
@@ -16,7 +16,7 @@ function Monogram({ highlight, className, ...props }) {
       <svg className={classNames('monogram', className)} width={40} height={40} viewBox="0 0 15 15" overflow="visible" onClick={enlarge} {...props}>
         {/* <image href={circle} x="-8" y="-8" width="27" height="27" className='logo-ink'/>   */}
         <text x="50%" y="50%" dy=".35em" textAnchor="middle" className='monogram__text monogram--animate'>
-          ネ
+          {/*ネ*/}N
         </text>
       </svg>
   );
