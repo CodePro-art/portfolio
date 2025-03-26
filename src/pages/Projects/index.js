00752092014d78b69602c5704c7d7d9948c725be
Netanel Mazuz <netazuz@gmail.com>
2025-03-26 11:43:01 +0200

    remove modal-certificates again

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -59,8 +59,8 @@ const Projects = () => {
             <TabsPanel />  
           </div>
 
-          <ModalCertificate />
-          <Certificates />          
+          {/* <ModalCertificate />
+          <Certificates />           */}
           
       </div>
       <Footer />
